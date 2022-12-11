e0dbb2d55a9558a7def1aa384964f7db47d8a066
TanhuKowski <khanishlielshad@gmail.com>
2022-12-11 15:53:47 +0400

    Login

--- Login_Page/RegisterPage.aspx.cs
+++ Login_Page/RegisterPage.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Web;
@@ -16,32 +17,27 @@ namespace Login_Page
         {
 
         }
-       
+
         protected void btn_register_Click(object sender, EventArgs e)
         {
             lblmsg.Text = "";
             string strcon = "Data Source = MSI\\SQLEXPRESS; Initial Catalog = ASP_Login; Integrated Security = True";
             SqlConnection con = new SqlConnection(strcon);
-            SqlCommand com = new SqlCommand("register", con);
-            com.CommandType = System.Data.CommandType.StoredProcedure;
-            SqlParameter p1 = new SqlParameter("username", txt_user_regs.Text);
-            SqlParameter p2 = new SqlParameter("password", txt_pass_regs.Text);
-            com.Parameters.Add(p1);
-            com.Parameters.Add(p2);
+            SqlCommand cmd = new SqlCommand("register", con);
             con.Open();
-            SqlDataReader rd = com.ExecuteReader();
-            if (rd.HasRows)
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@username", txt_user_regs.Text);
+            cmd.Parameters.AddWithValue("@password", txt_pass_regs.Text);
+            cmd.Parameters.Add(new SqlParameter()
             {
-                lblmsg.Text = "The User" + txt_user_regs.Text + "has been created!";
-                lblmsg.ForeColor = System.Drawing.Color.Green;
-                lblmsg.Font.Bold = true;
-            }
-            else
-            {
-                lblmsg.Text = "Registration failed!";
-                lblmsg.ForeColor = System.Drawing.Color.Red;
-                lblmsg.Font.Bold = true;
-            }
+                Direction = ParameterDirection.Output,
+                ParameterName = "@status",
+                SqlDbType = SqlDbType.Int
+            });
+            con.Close();
+            cmd.ExecuteNonQuery();
+            lblmsg.Text = "The user has been created!";
+
         }
 
         protected void btn_login_Click(object sender, EventArgs e)
